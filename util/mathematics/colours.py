1ef4b91f4c15a98e8e8d36f0b53e5840e91f96e0
Struan McDonough <Struan McDonough>
2016-12-18 16:11:28 +0000

    Overdue commit

---
+++ util/mathematics/colours.py
@@ -0,0 +1,10 @@
+def rgb_to_hex(r, g, b):
+    return '#%02x%02x%02x' % (r, g, b)
+
+def rgb(minimum, maximum, value):
+    minimum, maximum = float(minimum), float(maximum)
+    ratio = 2 * (value - minimum) / (maximum - minimum)
+    b = int(max(0, 255 * (1 - ratio)))
+    r = int(max(0, 255 * (ratio - 1)))
+    g = 255 - b - r
+    return r, g, b
