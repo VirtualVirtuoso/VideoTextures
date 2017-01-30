20d1f24df6a842833ee6305c91c1a26285510567
Struan <struanmcd@hotmaill.com>
2017-01-30 21:45:16 +0000

    Refactor the system because I keep getting lost

---
+++ analysis/determine_probabilities.py
@@ -0,0 +1,99 @@
+import copy
+import numpy
+import math
+from gui.frame_chooser import create_matrix_visualization
+
+from matplotlib import pyplot as plt
+
+threshold_value = 0.85
+
+
+
+# The purpose of this script is to take the existing similarity matrix, and then determine
+# a thresholded probability of whether or not a jump should happen. We then save this to a
+# file which is then read by the synthesis
+def main():
+
+    # Load the distance matrix from the file
+    distance_matrix = load_matrix_from_file("../data/output/difference_matrix.csv")
+
+    # Find the probabilities, threshold them, and then save them to a file
+    prob_matrix = create_probability_matrix(distance_matrix)
+    thresholded_matrix = threshold_matrix(copy.copy(prob_matrix), threshold_value)
+    numpy.savetxt("../data/output/thresholded_matrix.csv", thresholded_matrix, delimiter=",")
+
+    create_visualisations(prob_matrix, thresholded_matrix)
+
+
+
+# This is for the purposes of visualising the matrices produced by this stage. This is
+# useful because it both allows far easier debugging, but also acts as a visual aid for
+# demonstrating this system
+def create_visualisations(prob_matrix, thresholded_matrix):
+
+    # Map probabilities to gray pixels
+    grayscale_prob_matrix = prob_matrix * 255
+    grayscale_thresholded_matrix = thresholded_matrix * 255
+
+    # Show the normal distance matrix
+    show_matrix_gui(grayscale_prob_matrix, "Distance Matrix")
+
+    # Show the thresholded matrix
+    threshold_title = "Thresholded at: ", threshold_value
+    show_matrix_gui(grayscale_thresholded_matrix, threshold_title)
+
+    plt.imshow(grayscale_prob_matrix, interpolation='nearest')
+    plt.show(block=False)
+
+
+
+# The paper suggests mapping the distances determined earlier to probabilities using
+# the exponential function, in particular P_{ij} \propto exp(-D_{i+1,j}/\sigma)
+# Here, sigma is defined to be a mapping constant, which is suggested to be a small
+# multiple of the average D_{ij} values
+def create_probability_matrix(distance_matrix):
+
+    # Here, sigma is the mean * 6
+    sigma = float(distance_matrix.mean()) * 6
+    (height, width) = distance_matrix.shape
+
+    # Initialise the matrix
+    prob_matrix = numpy.zeros((height - 1, width - 1))
+
+    # Determine each of the probabilities
+    for x in range(0, height - 1):
+        for y in range(0, width - 1):
+            prob_matrix[x][y] = math.exp((-distance_matrix[x + 1][y]) / sigma)
+
+    return prob_matrix
+
+
+
+# Here we take the probability matrix, and threshold it against a certain value.
+def threshold_matrix(matrix, threshold):
+    thresholded_matrix = matrix
+    low_value_indices = thresholded_matrix < threshold
+    thresholded_matrix[low_value_indices] = 0
+    return thresholded_matrix
+
+def normalize_by_rows(matrix):
+    (height, width) = matrix.shape
+    normalized_matrix = numpy.zeros((height, width))
+
+    for x in range(0, height):
+        row = matrix[x]
+        euclidean_dist = numpy.atleast_1d(numpy.linalg.norm(row, 2, -1))
+        euclidean_dist[euclidean_dist == 0] = 1
+        normalized_matrix[x] = row / numpy.expand_dims(euclidean_dist, -1)
+
+    return normalized_matrix
+
+def load_matrix_from_file(file_name):
+    matrix = numpy.loadtxt(open(file_name, "rb"), delimiter=",")
+    return matrix
+
+def show_matrix_gui(matrix, title):
+    create_matrix_visualization(matrix, title)
+
+if __name__ == "__main__":
+    main()
--- analysis/determine_similarities.py
+++ analysis/determine_similarities.py
@@ -3,16 +3,23 @@ import numpy
 import util.video.keyframes as keyframes
 import util.frame.compare as compare
 
+# This is the first pre-processing step we use to determine the similarities between
+# each frame of the video. We then store this in a matrix, which we can then turn into
+# probabilities and threshold in the next step.
 def main():
     print "Determining similarities between frames..."
     build_similarity_matrix("data/shorter.mov")
 
 def build_similarity_matrix(video):
+
+    # We create two video objects, each of which will scan one frame
     video_1 = cv2.VideoCapture(video)
     video_2 = cv2.VideoCapture(video)
 
+    # We then determine the number of frames, so we can iterate to that point
     frame_count = keyframes.count_frames(video)
 
+    # Instantiate the matrix in which we will determine the similarities
     sim_matrix = numpy.zeros((frame_count, frame_count))
 
     # Go through each of the frames, and find their similarity with others
@@ -26,9 +33,10 @@ def build_similarity_matrix(video):
             video_2.set(cv2.cv.CV_CAP_PROP_POS_FRAMES, y + 1)
             (y_success, frame_y) = video_2.read()
 
+            # We make our comparison based on the Euclidean (L2) norm
             sim_matrix[x, y] = compare.euclidean_norm(frame_x, frame_y)
 
-
+    # Save this to a file, so we can process the results later
     print "Saved output to 'data/output/difference_matrix.csv'... "
     numpy.savetxt("data/output/difference_matrix.csv", sim_matrix, delimiter=",")
 
