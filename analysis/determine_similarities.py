8b3b3cd4fdc8bbf927bdadd0436423b77cceec92
Struan <struanmcd@hotmaill.com>
2017-02-04 22:25:29 +0000

    Allow the application to "skip frames" for larger inputs

--- analysis/determine_similarities.py
+++ analysis/determine_similarities.py
@@ -3,12 +3,14 @@ import numpy
 import util.video.keyframes as keyframes
 import util.frame.compare as compare
 
+skipFrames = 5
+
 # This is the first pre-processing step we use to determine the similarities between
 # each frame of the video. We then store this in a matrix, which we can then turn into
 # probabilities and threshold in the next step.
 def main():
     print "Determining similarities between frames..."
-    build_similarity_matrix("data/shorter.mov")
+    build_similarity_matrix("data/jurassic.mov")
 
 def build_similarity_matrix(video):
 
@@ -20,21 +22,21 @@ def build_similarity_matrix(video):
     frame_count = keyframes.count_frames(video)
 
     # Instantiate the matrix in which we will determine the similarities
-    sim_matrix = numpy.zeros((frame_count, frame_count))
+    sim_matrix = numpy.zeros((frame_count / skipFrames, frame_count / skipFrames))
 
     # Go through each of the frames, and find their similarity with others
-    for x in range(0, frame_count - 1):
+    for x in range(0, frame_count - 1, skipFrames):
         print "Analyzing frame ", x + 1, "/", frame_count, " ..."
 
         video_1.set(cv2.cv.CV_CAP_PROP_POS_FRAMES, x + 1)
         (x_success, frame_x) = video_1.read()
 
-        for y in range(0, frame_count - 1):
+        for y in range(0, frame_count - 1, skipFrames):
             video_2.set(cv2.cv.CV_CAP_PROP_POS_FRAMES, y + 1)
             (y_success, frame_y) = video_2.read()
 
             # We make our comparison based on the Euclidean (L2) norm
-            sim_matrix[x, y] = compare.euclidean_norm(frame_x, frame_y)
+            sim_matrix[x / skipFrames, y / skipFrames] = compare.euclidean_norm(frame_x, frame_y)
 
     # Save this to a file, so we can process the results later
     print "Saved output to 'data/output/difference_matrix.csv'... "
