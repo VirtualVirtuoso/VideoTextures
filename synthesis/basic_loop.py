8e9c5efda4d939fdb81f6e18b40680ca559fb475
Struan <struanmcd@hotmaill.com>
2017-02-15 18:33:26 +0000

    Begin basic synthesis work

---
+++ synthesis/basic_loop.py
@@ -0,0 +1,43 @@
+import cv2
+
+import config as c
+import definitions as d
+import util.video.keyframes as kf
+
+
+def main():
+
+    (cap, position_frame) = kf.source_video(c.absInputPath)
+
+    window_name = d.basic_loop_play
+    cv2.namedWindow(window_name, cv2.WINDOW_OPENGL)
+    cv2.startWindowThread()
+
+    while True:
+        flag, frame = cap.read()
+
+        if flag:
+            cv2.imshow(window_name, frame)
+            position_frame = cap.get(cv2.cv.CV_CAP_PROP_POS_FRAMES)
+        else:
+            cap.set(cv2.cv.CV_CAP_PROP_POS_FRAMES, position_frame - 1)
+            print d.frame_not_ready
+            cv2.waitKey(1000)
+
+        # Terminate the program if ESC is pressed
+        if cv2.waitKey(10) == 27:
+            cv2.destroyAllWindows()
+            break
+
+        # Terminate the program if the window is closed
+        if cv2.getWindowProperty(window_name, 0) == -1:
+            cv2.destroyAllWindows()
+            break
+
+        if cap.get(cv2.cv.CV_CAP_PROP_POS_FRAMES) == cap.get(cv2.cv.CV_CAP_PROP_FRAME_COUNT):
+            cap.set(cv2.cv.CV_CAP_PROP_POS_FRAMES, 0)
+
+
+
+if __name__ == "__main__":
+    main()
