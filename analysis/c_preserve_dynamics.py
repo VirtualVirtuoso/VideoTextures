ddf308cf86af6f9c59550c3c053c57e55923d7ec
Struan <struanmcd@hotmaill.com>
2017-02-19 22:31:12 +0000

    Remove redundancy from the main functions

--- analysis/c_preserve_dynamics.py
+++ analysis/c_preserve_dynamics.py
@@ -1,6 +1,19 @@
-def main():
-    return "Hello"
+import numpy
+import math
 
+import config as c
+import util.mathematics.matrix as matrix_util
+
+def main():
+    probability_matrix = matrix_util.load_matrix("../data/output/probability_matrix.csv")
+    dynamic_matrix = preserve_dynamics(probability_matrix)
+    matrix_util.save_matrix(dynamic_matrix, "dynamic_matrix")
+
+    if c.displayVisualisations:
+        matrix_util.display_matrix(dynamic_matrix, "Dynamic Preserved Matrix")
+
+def preserve_dynamics(matrix):
+    return "Boop"
 
 if __name__ == "__main__":
     main()
--- analysis/e_prune_transitions.py
+++ analysis/e_prune_transitions.py
@@ -1,24 +1,16 @@
 import copy
-import numpy
 
 import util.mathematics.matrix as matrix_util
 import config as c
 
-from gui.frame_chooser import create_matrix_visualization
-
 def main():
-    future_matrix = matrix_util.load_matrix_from_file("../data/output/future_cost_matrix.csv")
+    future_matrix = matrix_util.load_matrix("../data/output/future_cost_matrix.csv")
     thresholded_matrix = matrix_util.threshold_matrix(copy.copy(future_matrix), c.thresholdValue)
-
-    numpy.savetxt("../data/output/thresholded_matrix.csv", thresholded_matrix, delimiter=",")
-
-    threshold_title = "Thresholded at: ", c.thresholdValue
-
-    thresholded_matrix /= numpy.max(numpy.abs(thresholded_matrix), axis=0)
-    thresholded_matrix *= 255.0
+    matrix_util.save_matrix(thresholded_matrix, "thresholded_matrix")
 
     if c.displayVisualisations:
-        create_matrix_visualization(thresholded_matrix, threshold_title)
+        threshold_title = "Thresholded at: ", c.thresholdValue
+        matrix_util.display_matrix(thresholded_matrix, threshold_title)
 
 if __name__ == "__main__":
     main()
