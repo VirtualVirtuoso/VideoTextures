d17fdd41f6c9a87de785865ea327bdf8097b3dbf
Struan <struanmcd@hotmaill.com>
2017-02-20 10:21:43 +0000

    Create Grayscale matrix representation

--- config.py
+++ config.py
@@ -1,26 +1,29 @@
+import os
+
 # Similarities
 skipFrames = 1
-inputPath = "data/input/clock.mov"
-absInputPath = "C:\Users\Struan\PycharmProjects\TestProject\data\input\clock.mov"
+inputFile = "clock.mov"
+inputPath = "data/input/" + inputFile
+absInputPath = "C:\Users\Struan\PycharmProjects\TestProject\data\input\\" + inputFile
 
 # Probabilities
 sigmaMult = 2
 
 # Preserving dynamics
-adjacentFrames = 3
+adjacentFrames = 2
 
 # GUI
 displayVisualisations = True
 cellWidth = 2
 fontSize = 6
-islandColouring = False
+colouringType = "Gray"  # Island, Rainbow or Gray
 
 # Thresholding
-thresholdValue = 0.995
+thresholdValue = 0.9
 
 # Future Cost
-qualityTradeOff = 10
-futureCostWeight = 0.998
+qualityTradeOff = 2
+futureCostWeight = 0.999
 strictFuture = False  # If we hit a dead-end, do we terminate?
 
 
