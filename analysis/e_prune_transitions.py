428c7076a0be438d99a76e77fd826ce48aa3c491
Struan <struanmcd@hotmaill.com>
2017-03-02 12:32:16 +0000

    Implement Local Maxima Thresholding

--- analysis/e_prune_transitions.py
+++ analysis/e_prune_transitions.py
@@ -1,16 +1,47 @@
 import copy
+import numpy as np
 
 import util.mathematics.matrix as matrix_util
 import config as c
 
+from scipy.signal import argrelextrema
+
 def main():
     future_matrix = matrix_util.load_matrix("../data/output/probability_matrix.csv")
-    thresholded_matrix = matrix_util.threshold_matrix(copy.copy(future_matrix), c.thresholdValue)
-    matrix_util.save_matrix(thresholded_matrix, "thresholded_matrix")
+    threshold_title = "Not thresholded..."
+
+    # Determine Local Maxima
+    if c.useLocalMaxima:
+        future_matrix = determine_local_maxima(future_matrix)
+        threshold_title = "Local Maxima"
+
+    # Now Threshold
+    if c.useThreshold:
+        future_matrix = matrix_util.threshold_matrix(copy.copy(future_matrix), c.thresholdValue)
+        if c.useLocalMaxima:
+            threshold_title = "Local Maxima & Thresholded at " + str(c.thresholdValue)
+        else:
+            threshold_title = "Thresholded at " + str(c.thresholdValue)
+
+    matrix_util.save_matrix(future_matrix, "thresholded_matrix")
 
     if c.displayVisualisations:
-        threshold_title = "Thresholded at: ", c.thresholdValue
-        matrix_util.display_matrix(thresholded_matrix, threshold_title)
+        matrix_util.display_matrix(future_matrix, threshold_title)
+
+def determine_local_maxima(matrix):
+    thresholded_matrix = matrix
+    (height, width) = matrix.shape
+
+    # Go through each of the rows
+    for i in range(0, height):
+        maxima = argrelextrema(matrix[i], np.greater)[0]
+        for j in range(0, width):
+            if j not in maxima:
+                matrix[i][j] = 0
+
+    return thresholded_matrix
+
+
 
 if __name__ == "__main__":
     main()
